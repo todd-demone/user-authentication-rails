caad29038bea2240da27e0fc7eaf399f70ac287f
Todd Demone <todd.demone.dev@gmail.com>
2022-04-15 19:32:49 +0000

    add the 'create' action - it checks the inputted email and password and, if there is a match, creates a session by saving the user.id inside a browser cookie. If no match, then redirect back to login Also add the 'destroy' action - remove the user.id from the browser cookie and redirect to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    # if the user exists and the password is correct
+    if user && user.authenticate(params[:password])
+      # save the user id inside the browser cookie. This is how we keep the user
+      # logged in when they navigate around our website
+      session[:id] = user.id
+      redirect_to '/'
+    else
+      # if the user's login doesn't work, send them back to the login form
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    session[:id] = nil
+    redirect_to '/login'
   end
-  
+
 end
