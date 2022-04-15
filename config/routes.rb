511628a6e8a4af37dabcc6938602b78a2d3681b6
Todd Demone <todd.demone.dev@gmail.com>
2022-04-15 19:23:58 +0000

    add GET and POST routes for login (call sessions#new and sessions#create, respectively), add GET route for logout (calls sessions#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
   # The priority is based upon order of creation: first created -> highest priority.
